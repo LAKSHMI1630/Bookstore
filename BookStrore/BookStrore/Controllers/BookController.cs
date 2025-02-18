6dd31a8a0cba96187c1476f2cd53667f34fd4fc0
lakshmi.kondamuri90@gmail.com <lakshmi.kondamuri90@gmail.com>
2025-02-18 21:10:40 +1100

    Added Model

---
+++ BookStrore/BookStrore/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using BookStrore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using BookStrore.Models;
+
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _repo;
+
+        public BookController()
+        {
+            _repo = new BookRepository();
+          
+        }
+        public List<Book> GetAllBooks ()
+        {
+            return _repo.GetAllBooks();
+        }
+
+        public Book GetBook(int id)
+        {
+            return _repo.GetBookById(id);
+        }
+
+        public List<Book> SearchBook(string bookname, string author)
+        {
+            return _repo.SearchBook(bookname, author);
+        }
+    }
+}
---
+++ BookStrore/BookStrore/Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using BookStrore.Models;
+
+namespace BookStrore.Repository
+{
+    public class BookRepository
+    {
+        public List<Book> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public Book GetBookById(int id)
+        {
+            Book b = DataSource().Where(x => x.BookId== id).FirstOrDefault();
+            return b;
+        }
+
+        public List<Book> SearchBook(string title, string author)
+        {
+
+            List<Book> lst = DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(author)).ToList();
+            return lst ;
+        }
+
+        private List<Book> DataSource()
+        {
+            return new List<Book>()
+            {
+                new Book(){BookId =1, Title="ASP.NET", Author="ABC"},
+                new Book(){BookId =2, Title="MVC", Author="XYZ" },
+                new Book(){BookId =3, Title="WEBAPI", Author="ABC" },
+                new Book(){BookId =4, Title="C#", Author="XYZ"}
+            };
+        }
+    }
+}
