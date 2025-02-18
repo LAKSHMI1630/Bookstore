cd95c8261dd25f40602c9b50a0fc70358da6e253
lakshmi.kondamuri90@gmail.com <lakshmi.kondamuri90@gmail.com>
2025-02-18 13:38:56 +1100

    sync

---
+++ BookStrore/BookStrore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStrore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
