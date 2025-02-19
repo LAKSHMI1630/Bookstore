558d2fdd6040d98471d2656a337699913891422b
lakshmi.kondamuri90@gmail.com <lakshmi.kondamuri90@gmail.com>
2025-02-19 22:59:30 +1100

    New

--- BookStrore/BookStrore/Program.cs
+++ BookStrore/BookStrore/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
@@ -6,7 +8,15 @@ var app = builder.Build();
 
 app.UseStaticFiles();
 
+//app.UseStaticFiles(new StaticFileOptions()
+//{
+//    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory() , "")),
+//    RequestPath = ""
+//});
+
 //app.MapGet("/", () => "Hello world!");
+
+
 
 app.MapDefaultControllerRoute();
 app.Run();
